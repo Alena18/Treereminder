f798ea1946bae0fae91853691c02f6f85145bf73
Alena Baklanskaya <73777191+Alena18@users.noreply.github.com>
2024-12-08 17:48:35 +0000

    Initial commit

---
+++ treereminder/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDbxlc7REvsmwU7OE-v0xxfzUpvaebmmWk",
+  authDomain: "reminder-8a77f.firebaseapp.com",
+  projectId: "reminder-8a77f",
+  storageBucket: "reminder-8a77f.firebasestorage.app",
+  messagingSenderId: "232681110246",
+  appId: "1:232681110246:web:54de53354bddac9716d927",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
