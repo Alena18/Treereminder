e207e9bac63a5dcd15d4cdc4f69cb0220eb31d39
Alena Baklanskaya <73777191+Alena18@users.noreply.github.com>
2024-12-12 13:31:39 +0000

    Add style

--- treereminder/src/components/Login.jsx
+++ treereminder/src/components/Login.jsx
@@ -3,6 +3,7 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 import { toast } from "react-toastify";
 import { Navigate } from "react-router-dom";
+import Trees from "../public/trees64.svg";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -25,37 +26,43 @@ function Login() {
   }
 
   return (
-    <form onSubmit={handleLogin}>
-      <h3>Login</h3>
-      <div className="mb-3">
-        <label>Email address</label>
-        <input
-          type="email"
-          className="form-control"
-          placeholder="Enter email"
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Enter password"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <div className="d-grid">
-        <button type="submit" className="btn btn-primary">
-          Login
-        </button>
-      </div>
-      <p className="forgot-password text-right">
-        New user? <a href="/register">Register here</a>
-      </p>
-    </form>
+    <>
+      <img alt="Tree" src={Trees} style={{ marginBottom: "5%" }} />
+      <form onSubmit={handleLogin}>
+        <h3>Login</h3>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div
+          className="d-grid justify-content-center"
+          style={{ marginTop: "5%" }}
+        >
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+        </div>
+        <p className="forgot-password text-right" style={{ marginTop: "5%" }}>
+          New user? <a href="/register">Register here</a>
+        </p>
+      </form>
+    </>
   );
 }
 
--- treereminder/src/components/Register.jsx
+++ treereminder/src/components/Register.jsx
@@ -4,6 +4,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { setDoc, doc } from "firebase/firestore";
 import { toast } from "react-toastify";
 import { useNavigate, Link } from "react-router-dom";
+import Trees from "../public/trees64.svg";
 
 function Register() {
   const [email, setEmail] = useState("");
@@ -42,57 +43,63 @@ function Register() {
   };
 
   return (
-    <form onSubmit={handleRegister}>
-      <h3>Sign Up</h3>
-      <div className="mb-3">
-        <label>First name</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="First name"
-          onChange={(e) => setFname(e.target.value)}
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <label>Last name</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Last name"
-          onChange={(e) => setLname(e.target.value)}
-        />
-      </div>
-      <div className="mb-3">
-        <label>Email address</label>
-        <input
-          type="email"
-          className="form-control"
-          placeholder="Enter email"
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Enter password"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <div className="d-grid">
-        <button type="submit" className="btn btn-primary">
-          Sign Up
-        </button>
-      </div>
-      <p className="forgot-password text-right">
-        Already registered <Link to="/login">Login</Link>{" "}
-        {/* Use Link component */}
-      </p>
-    </form>
+    <>
+      <img alt="Tree" src={Trees} style={{ marginBottom: "5%" }} />
+      <form onSubmit={handleRegister}>
+        <h3>Sign Up</h3>
+        <div className="mb-3">
+          <label>First name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First name"
+            onChange={(e) => setFname(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label>Last name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Last name"
+            onChange={(e) => setLname(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div
+          className="d-grid justify-content-center"
+          style={{ marginTop: "5%" }}
+        >
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </div>
+        <p className="forgot-password text-right" style={{ marginTop: "5%" }}>
+          Already registered <Link to="/login">Login</Link>{" "}
+          {/* Use Link component */}
+        </p>
+      </form>
+    </>
   );
 }
 
